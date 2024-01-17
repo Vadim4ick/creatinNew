dfb92155f847abde882c1ca0b5983f87d713dd7e
Vadim Firulyov <firulvv@mail.ru>
2024-01-18 03:14:48 +0400

    tesst

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -5,30 +5,30 @@ import { notFound } from "next/navigation";
 
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export async function generateMetadata() {
-  const { servicesPage } = await gql.GetSeoServicesPage();
+// export async function generateMetadata() {
+//   const { servicesPage } = await gql.GetSeoServicesPage();
 
-  if (
-    !servicesPage.data ||
-    !servicesPage.data.attributes ||
-    !servicesPage.data.attributes.seo
-  ) {
-    // Добавьте проверку на существование нужных свойств
-    return null;
-  }
+//   if (
+//     !servicesPage.data ||
+//     !servicesPage.data.attributes ||
+//     !servicesPage.data.attributes.seo
+//   ) {
+//     // Добавьте проверку на существование нужных свойств
+//     return null;
+//   }
 
-  const metadata = {
-    title: servicesPage.data.attributes.seo.metaTitle,
-    description: servicesPage.data.attributes.seo.metaDescription,
-    keywords: servicesPage.data.attributes.seo.keywords,
-    robots: servicesPage.data.attributes.seo.metaRobots,
-    viewport: servicesPage.data.attributes.seo.metaViewport,
-    structuredData: servicesPage.data.attributes.seo.structuredData,
-    canonical: servicesPage.data.attributes.seo.canonicalURL,
-  };
+//   const metadata = {
+//     title: servicesPage.data.attributes.seo.metaTitle,
+//     description: servicesPage.data.attributes.seo.metaDescription,
+//     keywords: servicesPage.data.attributes.seo.keywords,
+//     robots: servicesPage.data.attributes.seo.metaRobots,
+//     viewport: servicesPage.data.attributes.seo.metaViewport,
+//     structuredData: servicesPage.data.attributes.seo.structuredData,
+//     canonical: servicesPage.data.attributes.seo.canonicalURL,
+//   };
 
-  return metadata;
-}
+//   return metadata;
+// }
 
 const ServicesPage = async () => {
   const { serviceNames } = await gql.GetServicesNames();
