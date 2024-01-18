849a3798e50e2c403711c7ddd97a464b70909f7d
Vadim Firulyov <firulvv@mail.ru>
2024-01-18 11:49:51 +0400

    fix shriftt

--- src/app/services/[service]/[id]/page.tsx
+++ src/app/services/[service]/[id]/page.tsx
@@ -2,30 +2,30 @@ import { gql } from "@/graphql/client";
 import { ServiceCollection } from "@/page/ServiceCollection";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata() {
-  const { serviceCollection } = await gql.GetSeoServiceCollectionPage();
+// export async function generateMetadata() {
+//   const { serviceCollection } = await gql.GetSeoServiceCollectionPage();
 
-  if (
-    !serviceCollection.data ||
-    !serviceCollection.data.attributes ||
-    !serviceCollection.data.attributes.seo
-  ) {
-    // Добавьте проверку на существование нужных свойств
-    return null;
-  }
+//   if (
+//     !serviceCollection.data ||
+//     !serviceCollection.data.attributes ||
+//     !serviceCollection.data.attributes.seo
+//   ) {
+//     // Добавьте проверку на существование нужных свойств
+//     return null;
+//   }
 
-  const metadata = {
-    title: serviceCollection.data.attributes.seo.metaTitle,
-    description: serviceCollection.data.attributes.seo.metaDescription,
-    keywords: serviceCollection.data.attributes.seo.keywords,
-    robots: serviceCollection.data.attributes.seo.metaRobots,
-    viewport: serviceCollection.data.attributes.seo.metaViewport,
-    structuredData: serviceCollection.data.attributes.seo.structuredData,
-    canonical: serviceCollection.data.attributes.seo.canonicalURL,
-  };
+//   const metadata = {
+//     title: serviceCollection.data.attributes.seo.metaTitle,
+//     description: serviceCollection.data.attributes.seo.metaDescription,
+//     keywords: serviceCollection.data.attributes.seo.keywords,
+//     robots: serviceCollection.data.attributes.seo.metaRobots,
+//     viewport: serviceCollection.data.attributes.seo.metaViewport,
+//     structuredData: serviceCollection.data.attributes.seo.structuredData,
+//     canonical: serviceCollection.data.attributes.seo.canonicalURL,
+//   };
 
-  return metadata;
-}
+//   return metadata;
+// }
 
 const ServiceParamsPage = async ({
   params,
