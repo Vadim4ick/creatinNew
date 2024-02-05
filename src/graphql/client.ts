2cfe19db90c29f9a9022cc84bac4b65500afc066
Vadim Firulyov <firulvv@mail.ru>
2024-02-06 00:45:45 +0400

    adding no-cache

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -3,6 +3,7 @@ import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./__generated__";
 
 const client = new GraphQLClient(`${process.env.STRAPI_URL}/graphql`, {
+  cache: "no-cache",
   headers: {
     Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
   },
