6acd4a040394571dfb61b8125f8986f2c7bb597d
Vadim Firulyov <firulvv@mail.ru>
2024-04-23 23:58:00 +0400

    adding new 25.04

--- src/components/OfferTopBanner/ui/OfferTopBanner.tsx
+++ src/components/OfferTopBanner/ui/OfferTopBanner.tsx
@@ -3,6 +3,7 @@ import cls from "./OfferTopBanner.module.scss";
 import { useMedia } from "@/shared/hooks/useMedia";
 import ReactMarkdown from "react-markdown";
 import { handleContextMenu } from "@/shared/helpers/handleContenxtMenu";
+import React from "react";
 
 const OfferTopBanner = ({
   banner,
@@ -27,8 +28,25 @@ const OfferTopBanner = ({
         <ReactMarkdown
           skipHtml
           components={{
-            p: ({ children }) => <h3>{children}</h3>,
             strong: ({ children }) => <b>{children}</b>,
+            p: (props) => {
+              // @ts-ignore
+              const cleanedData = props.children.map((item, index) => {
+                if (typeof item === "string") {
+                  // Заменяем '\n' на перенос строки
+                  const lines = item.split("\n");
+                  return lines.map((line, i) => (
+                    <React.Fragment key={i}>
+                      {line}
+                      {i < lines.length - 1 && <br />}
+                    </React.Fragment>
+                  ));
+                }
+                return item;
+              });
+
+              return <h3>{cleanedData}</h3>;
+            },
           }}
         >
           {banner.title}
