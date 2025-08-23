c83232dd04a5b898ed7884467f209b9215bdb80e
Vadim Firulyov <firulvv@mail.ru>
2025-08-23 11:53:16 +0400

    fix bags

--- src/app/_section/Cases.tsx
+++ src/app/_section/Cases.tsx
@@ -3,12 +3,9 @@
 import { Case } from "@/components/Case";
 import { GetHomePageQuery } from "@/graphql/__generated__";
 import { getRoutePortfolio } from "@/shared/const/pages";
-import useIntersectionObserver from "@/shared/hooks/useIntersectionObserver";
-import { SplitTypeAnimation } from "@/shared/hooks/useSplitTypeAnimation";
 import { BtnArrowThird } from "@/shared/icons/BtnArrowThird";
 import { ButtonDetails } from "@/shared/ui/ButtonDetails";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
 
 interface CasesProps {
   cases: GetHomePageQuery["homePage"]["data"]["attributes"]["cases"];
@@ -20,20 +17,10 @@ const Cases = (props: CasesProps) => {
 
   const router = useRouter();
 
-  const titleRef = useRef(null);
-
-  useIntersectionObserver({
-    ref: titleRef,
-  });
-
   return (
     <section className={`cases ${className}`}>
       <div className="cases__container">
-        <SplitTypeAnimation bg="#aaaaaa" fg="#181818" refChar={titleRef}>
-          <h2 ref={titleRef} className="cases__title _title" data-observe>
-            наши кейсы
-          </h2>
-        </SplitTypeAnimation>
+        <h2 className="cases__title">Наши партнёры</h2>
 
         <div className="cases__row">
           {cases.data.map((el) => (
